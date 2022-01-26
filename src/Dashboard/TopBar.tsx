2311748742433aed7001f1a240e5269da7cb2f6e
Johannes Schüth <git@jotschi.de>
2022-01-27 00:20:14 +0100

    Add layout changes

--- src/Dashboard/TopBar.tsx
+++ src/Dashboard/TopBar.tsx
@@ -6,7 +6,6 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
-import MenuIcon from "@mui/icons-material/Menu";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
@@ -17,6 +16,12 @@ import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import SearchIcon from "@mui/icons-material/Search";
 import Container from "@mui/material/Container";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import MailIcon from "@mui/icons-material/Mail";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MoreIcon from "@mui/icons-material/MoreVert";
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
@@ -159,8 +164,8 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   paddingBottom: 0,
   "@media all": {
     minHeight: 100,
-    paddingLeft: "20px",
-    paddingRight: "20px",
+    paddingLeft: "110px",
+    paddingRight: "0px",
   },
 }));
 
@@ -169,6 +174,105 @@ export default function TopBar({ open, setOpen }: TopBarProps) {
     setOpen(!open);
   };
   const theme = useTheme();
+
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
     <AppBar position="fixed" open={open}>
@@ -185,56 +289,83 @@ export default function TopBar({ open, setOpen }: TopBarProps) {
           }),
         }}
       >
-        <Grid container spacing={0}>
-          <Grid
-            item
-            xs={1}
-            sx={{
-              ...(open && { display: "none" }),
-            }}
-          >
+        <Grid container spacing={0} sx={{
+          
+        }}>
+          <Grid item xs={12}>
+          <Grid container>
             <IconButton
+              size="large"
               edge="start"
               color="inherit"
               aria-label="open drawer"
               onClick={toggleDrawer}
+              sx={{ mr: 2,
+                ...(open && { display: "none" })
+              }}
             >
               <MenuIcon />
             </IconButton>
-          </Grid>
-          <Grid
-            item
-            xs={9}
-            sx={{
-              ...(!open && { paddingLeft: "10px" }),
-            }}
-          >
-            <MainSearchBar />
-          </Grid>
-          <Grid item xs={1} />
-          <Grid item xs={1} sx={{ backgroundColor: "red" }}>
-            <IconButton
-              color="inherit"
-              sx={{ flexGrow: 1, alignSelf: "flex-end" }}
-              size="large"
-              aria-label="display more actions"
-              edge="end"
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" } }}
             >
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
+              Loom
+            </Typography>
+            <Grid item xs={6}>
+            <MainSearchBar/>
+            </Grid>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                aria-label="show 4 new mails"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <MailIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={17} color="error">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+            </Grid>
           </Grid>
-
-          {/*Second Row*/}
-          <Grid position="relative"
-            item
-            xs={12}
-            sx={{
-              left: 90,
-              backgroundColor: "green",
-            }}
-          >
+          <Grid sx={{ width: "100%" }} />
+          <Grid item xs={12}>
+            {/*Second Row*/}
             <BreadcrumbArea />
           </Grid>
         </Grid>
